c93b0d13ed90dc4740f8549c755d5331dfaed648
PokeStorm12 <pokestorm85@gmail.com>
2026-05-28 15:10:37 -0300

    Add files via upload

---
+++ tickets.js
@@ -0,0 +1,7 @@
+<h1>Abrir Ticket</h1>
+
+<iframe 
+  src="https://docs.google.com/forms/d/e/1FAIpQLScavcxLxyVcJXQ6R0RjGzlX46sj2t-7bWSPEIltKTyTOYW8bw/viewform?embedded=true"
+  width="100%" 
+  height="800">
+</iframe>
